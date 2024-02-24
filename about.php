067c230cc6069a92a8dd55c2b2affe183a822ac0
Nastin Gwaza <nastinmfena@gmail.com>
2024-02-24 20:03:11 +0800

    Initial commit

---
+++ about.php
@@ -0,0 +1,27 @@
+ <!-- Masthead-->
+        <header class="masthead">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-10 align-self-end mb-4" style="background: #0000002e;">
+                    	 <h1 class="text-uppercase text-white font-weight-bold">About Us</h1>
+                        <hr class="divider my-4" />
+                    </div>
+                    
+                </div>
+            </div>
+        </header>
+
+    <section class="page-section">
+        <div class="container">
+    <p> The University was established to provide high-quality tertiary education for the country’s ever-increasing number of secondary school leavers.
+
+ 
+
+At the time of formation, Nile University had three faculties (Arts & Social Sciences, Engineering, and Natural & Applied Sciences) and 93 students.
+
+ 
+
+As the University grew and thrived over the following years, the need to provide its high-quality instruction at a higher level became increasingly apparent. </p>        
+            
+        </div>
+        </section>
